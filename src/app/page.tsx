60c7bb229985267260b90d9d6598a4889e648b16
UpekshaHerath <upekshadilshan000@gmail.com>
2024-09-18 15:06:44 +0530

    feat: initial loading component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
-import { Button } from '@/components/ui/button';
+import { InitialLoadingComponent } from '@/components/InitialLoadingComponent';
 
 export default function Home() {
   return (
     <div>
-      <Button>Click me</Button>
+      <InitialLoadingComponent />
     </div>
   );
 }
---
+++ src/components/InitialLoadingComponent.tsx
@@ -0,0 +1,13 @@
+
+export const InitialLoadingComponent = () => {
+  return (
+    <div className="flex justify-center items-center min-h-screen bg-black">
+      <h1 className="relative text-8xl uppercase text-gray-800 tracking-widerleading-tight">
+        <span className="absolute top-0 left-0 w-full text-blue-500 overflow-hidden animate-letter-right-fill" data-text="FitSixes">
+          FitSixes
+        </span>
+        FitSixes
+      </h1>
+    </div>
+  );
+};
