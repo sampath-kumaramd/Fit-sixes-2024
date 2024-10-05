fe3f9338077a50b657ac585d93d0d7785826a977
Haritha Hasathcharu <42366642+hasathcharu@users.noreply.github.com.>
2024-10-05 19:40:06 +0530

    Add mobile responsiveness

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,9 +13,14 @@ const config = {
     container: {
       center: true,
       padding: '2rem',
-      screens: {
-        '2xl': '1400px',
-      },
+    },
+    screens: {
+      sd: '520px',
+      sm: '640px',
+      md: '768px',
+      lg: '1024px',
+      xl: '1280px',
+      '2xl': '1400px',
     },
     extend: {
       colors: {
