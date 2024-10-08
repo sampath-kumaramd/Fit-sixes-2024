4af5e948e11f2768fb9bf8d8ad31b2f86ec3fe49
janithmidissanayake <dissanayakejw.20@uom.lk>
2024-10-08 23:21:32 +0530

    contact us implementation

---
+++ src/components/ui/Footer.tsx
@@ -0,0 +1,73 @@
+"use client";
+
+import React from 'react'
+
+import Image from 'next/image'
+import Link from 'next/link';
+
+
+import MobileViewFooter from './mobileViewFooter';
+
+
+
+const Footer = () => {
+  return (
+    <div className='bg-[#030825] w-full '>
+         <footer className='max-w-screen-2xl mx-auto bg-gradient-to-r from-[#03081a] to-[#03082c] border-t border-gray-400 hidden sm:block '>
+        <div className=' w-full  text-white h-48 pt-8 '>
+            <div className='grid  grid-cols-4 sm:grid-cols-3 sm:mx-6 md:mx-8 lg:mx-16 xl:mx-32 '>
+            <div className=''>
+                <div className=''>
+                    <Image src={"/images/footerLogo.png"} alt='itfsu' width={150} height={150}/>
+                </div>
+                <div className='pt-[94px] ml-[165px] sm:ml-0 sm:pt-[12px] text-[6px] sm:text-sm whitespace-nowrap '>
+                    <p >Follow Us</p>
+                </div>
+                <div className='flex space-x-1 pt-3  ml-[165px] sm:ml-0 text-[6px] sm:text-sm '>
+                    <div>
+                        <Link href="https://www.facebook.com" target="_blank">
+                        <Image src={"/images/facebookLogo.png"} alt='Facebook' width={30} height={30} />
+                        </Link></div>
+                    <div>
+                    <Link href="https://www.instagram.com" target="_blank">
+                    <Image src={"/images/instagramLogo.png"} alt='Instagram' width={20} height={20} />
+                    </Link>
+                    </div>
+                    <div><Link href="https://www.tiktok.com" target="_blank">
+                     <Image src={"/images/tiktokLogo.png"} alt='TikTok' width={20} height={20} />
+                     </Link></div>
+                </div>
+            </div>
+            <div className='sm:pt-0 pt-6'>
+                <ul className='text-[5px] sm:text-[10px] flex flex-col text-center sm:text-left space-y-2'>
+                    <li>HOME</li>
+                    <li>ABOUT</li>
+                    <li>WATCH LIVE</li>
+                    <li>LIVE SCORE</li>
+                    <li>CONTACT US</li>
+
+                </ul>
+            </div>
+            <div className=' col-span-2 sm:col-span-1 flex'>
+                <div className=' flex flex-col items-center    text-center sm:text-[10px]'>
+                <div>Proudly Presented by</div>
+                <div>IT FACULTY STUDENTS&apos; UNION OF UNIVERSITY OF MORATUWA</div>
+                <div className='pt-2'><Image src={"/images/itfsuLogo.png"} alt='itfsu' width={100} height={100}/>
+                </div>
+                </div>
+              
+            </div>
+
+            </div>
+            <div className='text-[5px] sm:text-[8px] text-center pt-5 sm:pt-8'>2024@FIT SIXES - ALL RIGHT RESERVED</div>
+            
+        </div>
+
+    </footer>
+    <MobileViewFooter/>
+    </div>
+   
+  )
+}
+
+export default Footer
---
+++ src/components/ui/header.tsx
@@ -0,0 +1,49 @@
+"use client";
+
+import React, { useState } from 'react'
+
+import { AlignJustify, X } from 'lucide-react';
+import Image from 'next/image'
+
+import Navbar from './Navbar';
+
+
+
+const Header = () => {
+  const [isClick, setClick] = useState(false);
+  const toggleNavbar = () => {
+    setClick(!isClick);
+  }
+
+  return (
+    <div>
+      <div className='bg-[#03082a] w-full py-6'>
+        <div className='max-w-screen-2xl mx-auto flex justify-between items-center'>
+          <div className='mx-6 sm:mx-8 md:mx-12 lg:mx-16 xl:mx-32 text-white'>
+            <Image src="/images/logo.png" alt="logo" width={45} height={45} />
+          </div>
+          <div className='bg-[#03082a] mx-6 sm:mx-8 md:mx-12 lg:mx-16 xl:mx-32 hidden sm:block'>
+            <Navbar />
+          </div>
+          <div className='sm:hidden flex items-center'>
+            <button className='text-white inline-flex items-center justify-center p-2 hover:text-white focus:outline-none' onClick={toggleNavbar}>
+              {isClick ? (<X />) : (<AlignJustify />)}
+            </button>
+          </div>
+        </div>
+      </div>
+
+      {isClick && (
+        <div className='sm:hidden block'>
+          <div className='px-3 py-3 space-y-3 bg-[#03082a] text-white flex flex-col'>
+            <a href='/about' className=''>About</a>
+            <a href='/liveScore' className=''>Live Score</a>
+            <a href='/contactUs' className=''>Contact Us</a>
+          </div>
+        </div>
+      )}
+    </div>
+  )
+}
+
+export default Header;
