c87cb0e95a3e4ae9697361cec278884413c439b8
Sampath_Kumara <84168369+sampath-kumaramd@users.noreply.github.com>
2024-10-14 08:51:31 +0530

    update google analitics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,6 +27,24 @@ export default function RootLayout({
         <link rel="icon" href="/logo/logo-light.svg" />
       <link href="https://unpkg.com/@pqina/flip/dist/flip.min.css" rel="stylesheet"></link>
         <Script src="https://unpkg.com/@pqina/flip/dist/flip.min.js"></Script>
+        <Script
+          strategy="afterInteractive"
+          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_ID}`}
+        />
+        <Script
+          id="gtag-init"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', '${process.env.NEXT_PUBLIC_GA_ID}', {
+                page_path: window.location.pathname,
+              });
+            `,
+          }}
+        />
       </head>
       <body className={oswald.className}>
         <HomePageLayout>
